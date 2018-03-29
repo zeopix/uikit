bc89b5d9817cd32543fabaf07fcc801bf1184638
jms <jm@audio.d3ck.net>
2018-03-29 09:47:48 +0200

    _demo.html is test entry

---
+++ tests/js/unit/accordion.spec.js
@@ -0,0 +1,12 @@
+/* eslint-env jasmine */
+
+describe('accordion', () => {
+
+    it('test1', () => {
+        alert('I\'m testing');
+    });
+
+    it('test2', () => {
+        alert('I\'m still testing');
+    });
+});
