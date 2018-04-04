f94be64c78dd0d021409cd0677589389e493ada8
jms <jm@audio.d3ck.net>
2018-04-04 09:53:00 +0200

    moved bootstrapping to runner script

--- nightwatch.globals.js
+++ nightwatch.globals.js
@@ -1,14 +1,4 @@
 /* eslint-env node */
-const chromedriver = require('chromedriver');
-// const httpServer = require('http-server');
-const browserstack = require('browserstack-local');
-const nightwatch = require('nightwatch');
-
-let bsLocal = null;
-
-if (~process.argv.indexOf('--bs') || process.env.NIGHTWATCH_BROWSERSTACK) {
-    bsLocal = nightwatch.bs_local = new browserstack.Local;
-}
 
 const port = 8080;
 const host = 'uikit.test';
@@ -16,66 +6,19 @@ const rootUrl = `http://${host}:${port}/tests`;
 
 module.exports = {
 
-    // server: httpServer.createServer(), // {root:'./'}
-
     port,
 
     host,
 
     rootUrl,
 
-    before(done) {
-
-        try {
-            // this.server.listen(port, host);
-        } catch (e) {
-
-        }
-
-        if (bsLocal) {
-
-            bsLocal.start({}, err => {
-
-                if (err) {
-                    if (err.message.indexOf('another browserstack local')) {
-                        console.log('re-using running bs-local instance');
-                        done();
-                    } else {
-                        throw err.message;
-                    }
-                } else {
-                    done();
-                }
-
-            });
-
-        } else {
-            chromedriver.start();
-            done();
-        }
-
-    },
-
     after(done) {
-
-        // this.server.close();
-
-        if (bsLocal) {
-            bsLocal.stop(() => {
-                done();
-                process.exit();
-            });
-        } else {
-            chromedriver.stop();
-            done();
-            process.exit();
-        }
-
+        done();
+        process.exit();
     }
 
 };
 
-// kill browsers and stuf on crash/abort
-process.on('exit', function () {
+process.on('exit', function() {
     module.exports.after(() => {});
 });
--- nightwatch.runner.js
+++ nightwatch.runner.js
@@ -1,20 +1,74 @@
+/* eslint-env node */
+
 const conf = require('./nightwatch.conf');
 const globals = require('./nightwatch.globals');
 
 const cp = require('child_process');
 const httpServer = require('http-server');
-const server = httpServer.createServer(); // {root:'./'}
 
+const browserstack = require('browserstack-local');
+const nightwatch = require('nightwatch');
+const chromedriver = require('chromedriver');
+
+const server = httpServer.createServer(); // {root:'./'}
 const configs = Object.keys(conf.test_settings);
 
-server.listen(globals.port, globals.host);
+let bsLocal = null;
 
-const comand = './node_modules/.bin/nightwatch -e ' + configs.join(',') + ' ' + process.argv.slice(2).join(' ');
+if (~process.argv.indexOf('--bs') || process.env.NIGHTWATCH_BROWSERSTACK) {
+    bsLocal = nightwatch.bs_local = new browserstack.Local;
+}
 
-cp.exec(comand, {stdio: [0, 1, 2]}, (err, out) => {
-    console.log(out);
+beforeTests().then(doTests).then(afterTest);
+
+function beforeTests() {
+
+    server.listen(globals.port, globals.host);
+
+    return new Promise(res => {
+
+        if (bsLocal) {
+            bsLocal.start({}, err => {
+
+                if (err) {
+                    if (err.message.indexOf('another browserstack local')) {
+                        console.log('re-using running bs-local instance');
+                        doTests();
+                    } else {
+                        throw err.message;
+                    }
+                } else {
+                    res();
+                }
+
+            });
+        } else {
+            chromedriver.start();
+            res();
+        }
+    });
+}
+
+function doTests() {
+
+    return new Promise(res => {
+        const comand = './node_modules/.bin/nightwatch -e ' + configs.join(',') + ' ' + process.argv.slice(2).join(' ');
+        cp.exec(comand, {stdio: [0, 1, 2]}, (err, out) => {
+            console.log(out);
+
+            res();
+        }).on('error', err => {
+            throw err;
+        });
+    });
+}
+
+function afterTest() {
     server.close();
-}).on('error', err => {
-    throw err;
-});
+    if (bsLocal) {
+        bsLocal.stop(() => {});
+    } else {
+        chromedriver.stop();
+    }
+}
 
